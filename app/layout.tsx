52f7736c235f8997bbb65745b3c6eae37ef2c969
DarthKenar - 🏠 <federico.vega2222@gmail.com>
2026-05-02 00:23:10 -0300

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300..800;1,300..800&family=Cinzel:wght@400..900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300..800;1,300..800&family=Inter+Tight:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </head>
